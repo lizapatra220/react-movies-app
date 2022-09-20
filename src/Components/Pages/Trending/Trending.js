9e45f3bb256cca66fdeca898ae63e38d791f2562
lizapatra220 <lizapatra220@gmail.com>
2022-09-21 00:13:36 +0530

    theme for paging

--- src/Components/Pages/Trending/Trending.js
+++ src/Components/Pages/Trending/Trending.js
@@ -1,20 +1,23 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
+import Custompagination from "../../Pagination/Custompagination";
 import SingleContent from "../../SingleContent/SingleContent";
 import "./Trending.css";
 
 const Trending = () => {
+  const [page, setpage] = useState(1);
   const [content, setcontent] = useState([]);
   const fetching = async () => {
     const { data } = await axios.get(
-      `https://api.themoviedb.org/3/trending/all/day?api_key=9149e2ce082a8e7af1347082796ac1f0`
+      `https://api.themoviedb.org/3/trending/all/day?api_key=9149e2ce082a8e7af1347082796ac1f0 `
     );
     console.log(data.results);
     setcontent(data.results);
   };
   useEffect(() => {
+    window.scroll(0, 0);
     fetching();
-  }, []);
+  }, [page]);
 
   return (
     <div>
@@ -33,6 +36,7 @@ const Trending = () => {
             />
           ))}
       </div>
+      <Custompagination setpage={setpage} />
     </div>
   );
 };
--- src/Components/Pagination/Custompagination.js
+++ src/Components/Pagination/Custompagination.js
@@ -1,18 +1,33 @@
-import { Pagination } from "@mui/material";
+import { createTheme, Pagination, ThemeProvider } from "@mui/material";
 import React from "react";
+const darkTheme = createTheme({
+  palette: {
+    type: "dark",
+  },
+});
 
-const Custompagination = (page, pagecount = 10) => {
-  const handler = () => {
+const Custompagination = ({ setpage, pagecount = 10 }) => {
+  const handler = (page) => {
     setpage(page);
     window.scroll(0, 0);
   };
   return (
-    <div>
-      <Pagination
-        count={pagecount}
-        onChange={(e) => handler(e.target.textcontent)}
-      />
-      Custompagination
+    <div
+      style={{
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        marginTop: 10,
+      }}
+    >
+      <ThemeProvider theme={darkTheme}>
+        <Pagination
+          count={pagecount}
+          variant="outlined"
+          color="primary"
+          onChange={(e) => handler(e.target.textContent)}
+        />
+      </ThemeProvider>
     </div>
   );
 };
